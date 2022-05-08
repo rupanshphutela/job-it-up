5f5bf3879cb26f9b8a6483a3675af22bcfefc2a2
Ashlesha Bhamare <ashleshabhamare31@gmail.com>
2022-05-07 22:31:57 -0700

    Added updateJobPosterDetails method in JobPosterModel

--- model/JobPosterModel.js
+++ model/JobPosterModel.js
@@ -52,6 +52,54 @@ var JobPosterModel = /** @class */ (function () {
             response.json(itemArray);
         });
     };
+    JobPosterModel.prototype.updateJobPosterDetails = function (response, jobPosterId, body) {
+        this.model.findOneAndUpdate({ jobPosterId: jobPosterId }, body, function (err, jobPoster) {
+            if (!jobPoster)
+                console.log('Unable to fetch specified job poster');
+            else {
+                if (body.contactNo)
+                    jobPoster.contactNo = body.contactNo;
+                if (body.fname)
+                    jobPoster.fname = body.fname;
+                if (body.lname)
+                    jobPoster.lname = body.lname;
+                if (body.email)
+                    jobPoster.email = body.email;
+                if (body.referralCode)
+                    jobPoster.referralCode = body.referralCode;
+                if (body.location)
+                    jobPoster.location = body.location;
+                if (body.companyName)
+                    jobPoster.companyName = body.companyName;
+                if (body.picture)
+                    jobPoster.picture = body.picture;
+                if (body.companyLogo)
+                    jobPoster.companyLogo = body.companyLogo;
+                if (body.Overview)
+                    jobPoster.Overview = body.Overview;
+                if (body.website)
+                    jobPoster.website = body.website;
+                if (body.industry)
+                    jobPoster.industry = body.industry;
+                if (body.companySize)
+                    jobPoster.companySize = body.companySize;
+                if (body.headquarters)
+                    jobPoster.headquarters = body.headquarters;
+                if (body.founded)
+                    jobPoster.founded = body.founded;
+                jobPoster.save(function (err) {
+                    if (err) {
+                        console.log('error');
+                        response.send('Error : Job Poster details not updated');
+                    }
+                    else {
+                        console.log('success');
+                        response.send('Job Poster object updated');
+                    }
+                });
+            }
+        });
+    };
     return JobPosterModel;
 }());
 exports.JobPosterModel = JobPosterModel;
