6df3a168a9375de874f0a35d9cabad59dc3d586a
darshan <darshanradadiya98@gmail.com>
2022-05-21 01:28:20 -0700

    WIP: ""localhost:8080/app/job/:"" api integrated to make it dynamic.

--- src/app/applied-jobs/applied-jobs.component.ts
+++ src/app/applied-jobs/applied-jobs.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { JobClass } from '../job-class';
+import { JobItUpApisService } from '../job-it-up-apis.service';
 
 @Component({
   selector: 'app-applied-jobs',
   templateUrl: './applied-jobs.component.html',
-  styleUrls: ['./applied-jobs.component.css']
+  styleUrls: ['./applied-jobs.component.css'],
 })
 export class AppliedJobsComponent implements OnInit {
+  isJobSeeker!: String;
+  public results: Array<JobClass> = [];
 
-  constructor() { }
+  constructor(private apiService: JobItUpApisService) {}
 
   ngOnInit(): void {
+    this.isJobSeeker = 'Y';
+    this.apiService.getJobs().subscribe((result: JobClass[]) => {
+      this.results = result;
+    });
   }
 
-  deleteBtn(){
-    if(confirm('Are you sure to delete Application?'))
-    console.log('delete pressed....')
+  deleteBtn() {
+    if (confirm('Are you sure to delete Application?'))
+      console.log('delete pressed....');
     // call delete api here.
   }
-
 }
