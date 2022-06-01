7d87965f260d8bde8537d37a45520632a307eb99
Ashlesha Bhamare <ashleshabhamare31@gmail.com>
2022-05-31 20:50:53 -0700

    Added Mocha test for post job

---
+++ Mocha/jobItUp_tests/jobItUp_postJob.js
@@ -0,0 +1,43 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var async = require('async');
+
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+
+var http = require('http');
+chai.use(chaiHttp);
+
+describe('Post Job result', function () {
+
+    var requestResult;
+    var response;
+
+    before(function (done) {
+        chai.request("https://jobitup.azurewebsites.net/")
+            .post("/app/job")
+            .send({title: "Ux designer 2",skills : "javascript, java",domain : "Software industry", jobId :"1", jobPosterId :"1", location :"seattle", description :"This is a Ux designing job", salary :"15k", applydeadline :"07/12/2022",
+            startDate :"06/13/2022", endDate :"06/18/2022", experienceNeeded : "2 years", hasApplicants : "N"})
+            .end(function (err, res) {
+                requestResult = res.body;
+                response = res.text;
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                expect(response).to.be.a.string;
+                done();
+            });
+    });
+
+
+    it('the job post response has the key id', function(){
+        
+        expect(response).to.contain('{"id":"');
+	});
+
+    it('Job post response has key id and response length is greater than zero', function(){
+      
+        expect(response).to.contain('{"id":"').and.to.have.length.above(0);                    
+    });
+
+});
